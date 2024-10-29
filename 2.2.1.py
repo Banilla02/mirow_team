2f6fe082132fafba534bcb79b8b52ab7c4e4598a
DESKTOP-UBKLQP6\Owner <Banilla02123@gmail.com>
2024-10-29 16:28:07 +0900

    commit 2024.10.29

--- 2.2.1.py
+++ 2.2.1.py
@@ -31,8 +31,8 @@ for i in range(15):
 
     v1=v1//2
 
-l8 = []
-l9 = []
+PastWay = []
+PassingWay = []
 l10 = []
 l11 = []
 random_v1 = []
@@ -40,48 +40,48 @@ random_v2 = []
 random_v3 = []
 To_remove = []
 
-
-random_v1 = range(5)
-random_v2 = random.choices(random_v1,k=2)
-l8.append(random_v2)
-print(random_v1)
-print(random_v2)
-
-l9.append([l8[-1][0],l8[-1][1]+1])
-l9.append([l8[-1][0],l8[-1][1]-1])
-l9.append([l8[-1][0]+1,l8[-1][1]])
-l9.append([l8[-1][0]-1,l8[-1][1]])
-print(l8)
-print(l9)
-
 def start_list_remove(list_1,list_2):
+  To_remove = []
   for i in range(len(list_1)):
     if list_1[i][0] > 4 or list_1[i][1] > 4:
       To_remove.append(list_1[i])
     elif list_1[i][0] < 0 or list_1[i][1] < 0:
       To_remove.append(list_1[i])
-    for j in range(len(l8)):
+    for j in range(len(list_2)):
       if list_1[i][0] == list_2[j][0] and list_1[i][1] == list_2[j][1]:
-        To_remove.append(list[i])
+        To_remove.append(list_1[i]) 
+        
+  for i in range(len(To_remove)):
+    if To_remove[i] in list_1:
+      print("삭제할 값:",To_remove[i])
+      list_1.remove(To_remove[i])
 
-  for item in To_remove:
-    list_1.remove(item)
-  To_remove.clear
 
-start_list_remove(l9,l8)
-random_v3 = random.choice(l9)
-print(l9)
-print(random_v3)
+random_v1 = range(5)
+random_v2 = random.choices(random_v1,k=2)
+PastWay.append(random_v2)
+print("star_point :",random_v2)
+
+PassingWay.append([PastWay[-1][0],PastWay[-1][1]+1])
+PassingWay.append([PastWay[-1][0],PastWay[-1][1]-1])
+PassingWay.append([PastWay[-1][0]+1,PastWay[-1][1]])
+PassingWay.append([PastWay[-1][0]-1,PastWay[-1][1]])
+print("spread_start_point :",PastWay)
+print("will_passing_way",PassingWay)
+
+
+start_list_remove(PassingWay,PastWay)
+random_v3 = random.choice(PassingWay)
+print("passing way에서 갈 방향",random_v3)
 l10.append([random_v3[0],random_v3[1]+1])
 l10.append([random_v3[0],random_v3[1]-1])
 l10.append([random_v3[0]+1,random_v3[1]])
 l10.append([random_v3[0]-1,random_v3[1]])
-print(l10)
-start_list_remove(l10,l9)
-print(l10)
+start_list_remove(l10,PassingWay)
+print("",l10)
 for i in l10:
-  if l8 in l10:
-    l11.append(i)
+  if PastWay[i] in l10:
+    l11.append(PastWay[i])
 print(l11)
 
 
