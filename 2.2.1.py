52031d21c763c8ffd4e3114030e2dfce7589cc45
DESKTOP-UBKLQP6\Owner <Banilla02123@gmail.com>
2024-10-30 14:29:58 +0900

    commit 2024.10.30 -Banilla

--- 2.2.1.py
+++ 2.2.1.py
@@ -1,40 +1,30 @@
-#l2= 맵 구성
 import random
 
-#l2를 만들기 위한 과정
-l1=list([2]+[0]*3+[8])
-l2=[[3,1,1,1,9]]
-
-for i in range(3):
+l1=list([15]*5)
+l2=[]
+for i in range(5):
   l2.append(list(l1))
-
-l2.append([6,4,4,4,12])
 for i in l2:
   print(' '.join(map(str, i)))
-
-#l31(위),2(왼),4(아래),8(오른)
 l31=[]
 l32=[]
 l34=[]
 l38=[]
-
-
 for i in range(15):
   v1=8
   v2=i+1
   v3=int(v2)
-
   for i in range(4):
     if v3>=v1:
       globals()[f'l3{v1}'].append(v2)
       v3=v3-v1
-
     v1=v1//2
 
 PastWay = []
 PassingWay = []
 l10 = []
 l11 = []
+l12 = []
 random_v1 = []
 random_v2 = []
 random_v3 = []
@@ -56,7 +46,23 @@ def start_list_remove(list_1,list_2):
       print("삭제할 값:",To_remove[i])
       list_1.remove(To_remove[i])
 
+def Direct_inform(list_1,list_2):
+  if list_1[0] == list_2[0]:
+    if list_1[1]-1 == list_2[1]:
+      print('서')
+      
 
+    elif list_1[1]+1 == list_2[1]:
+      print('동')
+
+  elif list_1[0]-1 == list_2[0]:
+    print('북')
+
+  elif list_1[0]+1 == list_2[0]:
+    print('남')
+
+
+# start_algorism
 random_v1 = range(5)
 random_v2 = random.choices(random_v1,k=2)
 PastWay.append(random_v2)
@@ -66,11 +72,10 @@ PassingWay.append([PastWay[-1][0],PastWay[-1][1]+1])
 PassingWay.append([PastWay[-1][0],PastWay[-1][1]-1])
 PassingWay.append([PastWay[-1][0]+1,PastWay[-1][1]])
 PassingWay.append([PastWay[-1][0]-1,PastWay[-1][1]])
-print("spread_start_point :",PastWay)
+start_list_remove(PassingWay,PastWay)
 print("will_passing_way",PassingWay)
 
-
-start_list_remove(PassingWay,PastWay)
+#앞으로 나아갈 방향 정하기
 random_v3 = random.choice(PassingWay)
 print("passing way에서 갈 방향",random_v3)
 l10.append([random_v3[0],random_v3[1]+1])
@@ -78,10 +83,15 @@ l10.append([random_v3[0],random_v3[1]-1])
 l10.append([random_v3[0]+1,random_v3[1]])
 l10.append([random_v3[0]-1,random_v3[1]])
 start_list_remove(l10,PassingWay)
-print("",l10)
-for i in l10:
-  if PastWay[i] in l10:
-    l11.append(PastWay[i])
-print(l11)
+print("l10의 값",l10)
 
-
+for item in PastWay:
+  for i in l10:
+    if item == i:
+      l11.append(item)
+print('l10과 PastWay와 중복값',l11)
+random_v4 = random.choice(l11)
+l12.append([random_v3,random_v4])
+print(l12)
+Direct_inform(random_v3,random_v4)
+print(l2)
