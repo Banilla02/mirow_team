867bdc9f2729959fa7f637473ed54de2d6cc9829
DESKTOP-UBKLQP6\Owner <Banilla02123@gmail.com>
2024-10-31 12:29:59 +0900

    commit 2024.10.31 Banilla02

--- 2.2.1.py
+++ 2.2.1.py
@@ -43,24 +43,36 @@ def start_list_remove(list_1,list_2):
         
   for i in range(len(To_remove)):
     if To_remove[i] in list_1:
-      print("삭제할 값:",To_remove[i])
       list_1.remove(To_remove[i])
 
 def Direct_inform(list_1,list_2):
   if list_1[0] == list_2[0]:
     if list_1[1]-1 == list_2[1]:
       print('서')
-      
+      l2[random_v3[0]][random_v3[1]] = l2[random_v3[0]][random_v3[1]] - 2
+      l2[random_v4[0]][random_v4[1]] = l2[random_v4[0]][random_v4[1]] - 8
 
     elif list_1[1]+1 == list_2[1]:
       print('동')
+      l2[random_v3[0]][random_v3[1]] = l2[random_v3[0]][random_v3[1]] - 8
+      l2[random_v4[0]][random_v4[1]] = l2[random_v4[0]][random_v4[1]] - 2
 
   elif list_1[0]-1 == list_2[0]:
     print('북')
+    l2[random_v3[0]][random_v3[1]] = l2[random_v3[0]][random_v3[1]] - 1
+    l2[random_v4[0]][random_v4[1]] = l2[random_v4[0]][random_v4[1]] - 4
 
   elif list_1[0]+1 == list_2[0]:
     print('남')
+    l2[random_v3[0]][random_v3[1]] = l2[random_v3[0]][random_v3[1]] - 4
+    l2[random_v4[0]][random_v4[1]] = l2[random_v4[0]][random_v4[1]] - 1
 
+def remove_Re(data):
+  result = []
+  for i in data:
+    if i not in result:
+      result.append(i)
+  data = result
 
 # start_algorism
 random_v1 = range(5)
@@ -68,30 +80,42 @@ random_v2 = random.choices(random_v1,k=2)
 PastWay.append(random_v2)
 print("star_point :",random_v2)
 
-PassingWay.append([PastWay[-1][0],PastWay[-1][1]+1])
-PassingWay.append([PastWay[-1][0],PastWay[-1][1]-1])
-PassingWay.append([PastWay[-1][0]+1,PastWay[-1][1]])
-PassingWay.append([PastWay[-1][0]-1,PastWay[-1][1]])
-start_list_remove(PassingWay,PastWay)
-print("will_passing_way",PassingWay)
-
+while True:
+  PassingWay.append([PastWay[-1][0],PastWay[-1][1]+1])
+  PassingWay.append([PastWay[-1][0],PastWay[-1][1]-1])
+  PassingWay.append([PastWay[-1][0]+1,PastWay[-1][1]])
+  PassingWay.append([PastWay[-1][0]-1,PastWay[-1][1]])
+  start_list_remove(PassingWay,PastWay)
+  remove_Re(PassingWay)
+  print(PassingWay)
+  for i in range(5):
+    print(l2[i])
+  if 15 in l2:
+    break
 #앞으로 나아갈 방향 정하기
-random_v3 = random.choice(PassingWay)
-print("passing way에서 갈 방향",random_v3)
-l10.append([random_v3[0],random_v3[1]+1])
-l10.append([random_v3[0],random_v3[1]-1])
-l10.append([random_v3[0]+1,random_v3[1]])
-l10.append([random_v3[0]-1,random_v3[1]])
-start_list_remove(l10,PassingWay)
-print("l10의 값",l10)
+  random_v3 = []
+  random_v3 = random.choice(PassingWay)
+  l10 = []
+  l10.append([random_v3[0],random_v3[1]+1])
+  l10.append([random_v3[0],random_v3[1]-1])
+  l10.append([random_v3[0]+1,random_v3[1]])
+  l10.append([random_v3[0]-1,random_v3[1]])
+  start_list_remove(l10,PassingWay)
+  remove_Re(l10)
+  l11 = []
 
-for item in PastWay:
-  for i in l10:
-    if item == i:
-      l11.append(item)
-print('l10과 PastWay와 중복값',l11)
-random_v4 = random.choice(l11)
-l12.append([random_v3,random_v4])
-print(l12)
-Direct_inform(random_v3,random_v4)
-print(l2)
+  for item in PastWay:
+    for i in l10:
+      if item == i:
+        l11.append(item)
+  random_v4 = random.choice(l11)
+  l12.append([random_v3,random_v4])
+  Direct_inform(random_v3,random_v4)
+  PastWay.append(random_v3)
+  PassingWay.remove(random_v3)
+  print(PastWay)
+print(l2[0])
+print(l2[1])
+print(l2[2])
+print(l2[3])
+print(l2[4])
