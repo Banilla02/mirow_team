3b2314dcabc1c4b90ee416a8c947c2bde07689aa
DESKTOP-UBKLQP6\Owner <Banilla02123@gmail.com>
2024-11-20 05:17:57 +0900

    2024-11-20 error fix zeus leave t1.. is not real... i don't revlieves fact

--- 2.2.1.py
+++ 2.2.1.py
@@ -6,8 +6,11 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-num = 30
+num = 10 #맵의 크기
 
+#기본 맵 크기 정하기
+#맵 15로 채워넣기
+#실질적인 맵 = l2
 l1=list([15]*num)
 l2=[]
 for i in range(num):
@@ -27,16 +30,17 @@ for i in range(15):
       v3=v3-v1
     v1=v1//2
 
-PastWay = []
-PassingWay = []
-l10 = []
-l11 = []
+PastWay = [] #지나갔던 길
+PassingWay = [] #지나갈 길
+l10 = [] 
+l11 = [] 
 l12 = []
-random_v1 = []
-random_v2 = []
-random_v3 = []
-To_remove = []
+random_v1 = [] #처음시작지점
+random_v2 = [] #갈 길에서 랜덤
+random_v3 = [] #갔던 길에서 랜덤
+To_remove = [] #지울 리스트 값들
 
+#범위 이외의 값들 지우는 지정함수
 def start_list_remove(list_1,list_2):
   To_remove = []
   for i in range(len(list_1)):
@@ -52,6 +56,7 @@ def start_list_remove(list_1,list_2):
     if To_remove[i] in list_1:
       list_1.remove(To_remove[i])
 
+#기본 15에서 하나씩 벽을 지워서 길을 만드는 지정함수
 def Direct_inform(list_1,list_2):
   if list_1[0] == list_2[0]:
     if list_1[1]-1 == list_2[1]:
@@ -70,6 +75,7 @@ def Direct_inform(list_1,list_2):
     l2[random_v3[0]][random_v3[1]] = l2[random_v3[0]][random_v3[1]] - 4
     l2[random_v4[0]][random_v4[1]] = l2[random_v4[0]][random_v4[1]] - 1
 
+#중복되는 리스트 안에 값 제거하는 지정함수
 def remove_Re(data):
   result = []
   for i in data:
@@ -79,20 +85,20 @@ def remove_Re(data):
 
 # start_algorism
 random_v1 = range(num)
-random_v2 = random.choices(random_v1,k=2)
-PastWay.append(random_v2)
+random_v2 = random.choices(random_v1,k=2) #시작지점 정하기
+PastWay.append(random_v2) #시작지점을 갔던길 리스트에 추가하기
 
+#마지막으로 갔던 길에 앞으로 나아갈 길 추가하기
 while True:
   PassingWay.append([PastWay[-1][0],PastWay[-1][1]+1])
   PassingWay.append([PastWay[-1][0],PastWay[-1][1]-1])
   PassingWay.append([PastWay[-1][0]+1,PastWay[-1][1]])
   PassingWay.append([PastWay[-1][0]-1,PastWay[-1][1]])
-  start_list_remove(PassingWay,PastWay)
-  remove_Re(PassingWay)
+  start_list_remove(PassingWay,PastWay) #범위 외 값들 제거
+  remove_Re(PassingWay) #중복값들 지우기
   
+  #갈 길이 남아있지 않으면 while문 멈추기
   if not PassingWay:
-    print("end_point :",PastWay[-1])
-    for i in range(num):
     break
 
 #앞으로 나아갈 방향 정하기
@@ -107,16 +113,22 @@ while True:
   remove_Re(l10)
   l11 = []
 
+#두 리스트에 있는 값만 저장하기
   for item in PastWay:
     for i in l10:
       if item == i:
         l11.append(item)
+  
+  #갈수 있는 방향중에 하나만 정하기
   random_v4 = random.choice(l11)
   l12.append([random_v3,random_v4])
-  Direct_inform(random_v3,random_v4)
-  PastWay.append(random_v3)
-  PassingWay.remove(random_v3)
-  
+  Direct_inform(random_v3,random_v4) #방향에 따라 벽을 지우며 길을 만든다
+  PastWay.append(random_v3) #갔던 길에 길 하나를 추가한다.
+  PassingWay.remove(random_v3) #추가한 길을 중복되지 않게 갈 길에서 지워준다.
+
+#시작지점과 끝지점 뚫어주기
+l2[0][0]=l2[0][0]-2
+l2[-1][-1]=l2[-1][-1]-8
 
 # 미로 데이터 (비트 플래그 형식)
 maze = np.array(l2)
