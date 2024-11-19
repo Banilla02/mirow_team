cbbc1de07caa8b58c6468440b655fdcf1c00c3d1
DESKTOP-UBKLQP6\Owner <Banilla02123@gmail.com>
2024-11-19 16:31:03 +0900

    2024-11-19 commit Banilla02 #clear!!

--- 2.2.1.py
+++ 2.2.1.py
@@ -1,11 +1,18 @@
-import random
+#모듈 설치하기
+#pip install numpy
+#pip install matplotlib
 
-l1=list([15]*5)
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+num = 30
+
+l1=list([15]*num)
 l2=[]
-for i in range(5):
+for i in range(num):
   l2.append(list(l1))
-for i in l2:
-  print(' '.join(map(str, i)))
+
 l31=[]
 l32=[]
 l34=[]
@@ -33,7 +40,7 @@ To_remove = []
 def start_list_remove(list_1,list_2):
   To_remove = []
   for i in range(len(list_1)):
-    if list_1[i][0] > 4 or list_1[i][1] > 4:
+    if list_1[i][0] > num-1 or list_1[i][1] > num-1:
       To_remove.append(list_1[i])
     elif list_1[i][0] < 0 or list_1[i][1] < 0:
       To_remove.append(list_1[i])
@@ -71,10 +78,9 @@ def remove_Re(data):
   data = result
 
 # start_algorism
-random_v1 = range(5)
+random_v1 = range(num)
 random_v2 = random.choices(random_v1,k=2)
 PastWay.append(random_v2)
-print("start_point :",random_v2)
 
 while True:
   PassingWay.append([PastWay[-1][0],PastWay[-1][1]+1])
@@ -86,8 +92,7 @@ while True:
   
   if not PassingWay:
     print("end_point :",PastWay[-1])
-    for i in range(5):
-      print(l2[i])
+    for i in range(num):
     break
 
 #앞으로 나아갈 방향 정하기
@@ -112,19 +117,49 @@ while True:
   PastWay.append(random_v3)
   PassingWay.remove(random_v3)
   
-#미로 맵 시각화 하기
-#11*11map
-real_map = []
-O11= ['0','0','0','0','0','0','0','0','0','0','0']
-for i in range(11):
-  real_map.append(O11)
 
-for i in range(0,5,1):
-  print(i)
-  for j in range(0,5,1):
-    print(j)
-    real_map[i*2+1][j*2+1] = l2[i][j]
-  
+# 미로 데이터 (비트 플래그 형식)
+maze = np.array(l2)
 
-for i in range(len(real_map)):
-  print(real_map[i])
+# 방향별 비트 플래그 (위, 왼쪽, 아래, 오른쪽)
+DIRECTIONS = {
+    'UP': 1,     # 0001
+    'LEFT': 2,   # 0010
+    'DOWN': 4,   # 0100
+    'RIGHT': 8   # 1000
+}
+
+# 벽을 그리는 함수
+def draw_maze(maze):
+    fig, ax = plt.subplots(figsize=(6, 6))
+    
+    # 각 셀에 대해 벽을 그리기
+    for i in range(maze.shape[0]):
+        for j in range(maze.shape[1]):
+            cell = maze[i, j]
+            
+            # 위쪽 벽
+            if cell & DIRECTIONS['UP']:
+                ax.plot([j, j+1], [i, i], color='black', lw=2)
+            # 왼쪽 벽
+            if cell & DIRECTIONS['LEFT']:
+                ax.plot([j, j], [i, i+1], color='black', lw=2)
+            # 아래쪽 벽
+            if cell & DIRECTIONS['DOWN']:
+                ax.plot([j, j+1], [i+1, i+1], color='black', lw=2)
+            # 오른쪽 벽
+            if cell & DIRECTIONS['RIGHT']:
+                ax.plot([j+1, j+1], [i, i+1], color='black', lw=2)
+
+    # 기본 틀 제거: 축, 눈금, 격자선
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.set_aspect('equal')
+    ax.axis('off')  # 축을 아예 표시하지 않도록 설정
+
+    plt.show()
+
+# 미로 그리기
+draw_maze(maze)
